c4200091cdc96a859473d627a51307abc50ca892
Favour Olukayode <nerosilva522@gmail.com>
2021-11-10 11:24:59 +0100

    fix: adding Day 1 file

---
+++ Day1/Day1.md
@@ -0,0 +1,8 @@
+## Day 0
+
+The official start of the iOS journey. Successfully setup my development environment and had first walk through of the Xcode IDE.
+
+### Learned
+- Aspect rations in ImageViews
+- Cloned first iOS project
+- Connecting UI components to code using IBOutlets. Dragging and dropping to code view while the storyboard in assistant model.
---
+++ Day1/Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //IBOutlet connected to the object in storyboard by tapping and //dragging it to the code view after changing the view of the storyboard to assistant view
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
